91a5963d7161f203ffe40d00c1113bc970fb7a34
alexezm1 <alex_speed_96@hotmail.com>
2018-11-25 21:24:26 -0700

    as

--- models/proyecto.js
+++ models/proyecto.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Empleado = require('./user');
 const Cliente = require('./cliente')
+//
 
 const Schema = mongoose.Schema
     ObjectId = Schema.ObjectId
